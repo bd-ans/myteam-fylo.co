29505f342c5b4e689c3421277a7849f90ec141a7
Elayt <elaytpatters@gmail.com>
2022-02-28 18:41:09 +0500

    add 3th, 4th, 5th, 6th directors and fixed bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let plus_icon=document.getElementById("plus-icon");
-let plus_hover=document.getElementById("plus-hover");
-let plus_hoverth=document.getElementById("plus-hoverth");
+let plus_icon=document.getElementsByClassName("plus-icon")[0];
+let plus_hover=document.getElementsByClassName("plus-hover")[0];
+let plus_hoverth=document.getElementsByClassName("plus-hoverth")[0];
 
 plus_hover.style.transform="translate(0px, 0px)"
 plus_hoverth.style.transform="translate(0px, 0px)"
@@ -14,12 +14,142 @@ plus_icon.onclick=function() {
     plus_hoverth.style.transition="all 0.4s ease-in-out";
     plus_icon.style.transform="rotate(45deg)";
     plus_icon.style.transition="all 0.3s ease-in-out";
-
   } else {
     plus_hover.style.transform="translate(0px, 0px)";
     plus_hoverth.style.transform="translate(0px, 0px)";
     plus_icon.style.transform="rotate(0deg)";
     plus_icon.style.transition="all 0.3s ease-in-out";
+  }
+}
 
+
+// SEKOND
+
+let plus_icon2th=document.getElementsByClassName("plus-icon2th")[0];
+let plus_hover2th=document.getElementsByClassName("plus-hover2th")[0];
+let plus_hoverth2th=document.getElementsByClassName("plus-hoverth2th")[0];
+
+plus_hover2th.style.transform="translate(0px, 0px)"
+plus_hoverth2th.style.transform="translate(0px, 0px)"
+
+
+plus_icon2th.onclick=function() {
+  if(plus_hover2th.style.transform=="translate(0px, 0px)") {
+    plus_hover2th.style.transform="translate(0px, -256px)";
+    plus_hoverth2th.style.transform="translate(0px, -256px)";
+    plus_hover2th.style.transition="all 0.4s ease-in-out";
+    plus_hoverth2th.style.transition="all 0.4s ease-in-out";
+    plus_icon2th.style.transform="rotate(45deg)";
+    plus_icon2th.style.transition="all 0.3s ease-in-out";
+  } else {
+    plus_hover2th.style.transform="translate(0px, 0px)";
+    plus_hoverth2th.style.transform="translate(0px, 0px)";
+    plus_icon2th.style.transform="rotate(0deg)";
+    plus_icon2th.style.transition="all 0.3s ease-in-out";
+  }
+}
+
+
+// THIRTY 
+
+let plus_icon3th=document.getElementsByClassName("plus-icon3th")[0];
+let plus_hover3th=document.getElementsByClassName("plus-hover3th")[0];
+let plus_hoverth3th=document.getElementsByClassName("plus-hoverth3th")[0];
+
+plus_hover3th.style.transform="translate(0px, 0px)"
+plus_hoverth3th.style.transform="translate(0px, 0px)"
+
+
+plus_icon3th.onclick=function() {
+  if(plus_hover3th.style.transform=="translate(0px, 0px)") {
+    plus_hover3th.style.transform="translate(0px, -256px)";
+    plus_hoverth3th.style.transform="translate(0px, -256px)";
+    plus_hover3th.style.transition="all 0.4s ease-in-out";
+    plus_hoverth3th.style.transition="all 0.4s ease-in-out";
+    plus_icon3th.style.transform="rotate(45deg)";
+    plus_icon3th.style.transition="all 0.3s ease-in-out";
+  } else {
+    plus_hover3th.style.transform="translate(0px, 0px)";
+    plus_hoverth3th.style.transform="translate(0px, 0px)";
+    plus_icon3th.style.transform="rotate(0deg)";
+    plus_icon3th.style.transition="all 0.3s ease-in-out";
+  }
+}
+
+// FOURTH
+
+let plus_icon4th=document.getElementsByClassName("plus-icon4th")[0];
+let plus_hover4th=document.getElementsByClassName("plus-hover4th")[0];
+let plus_hoverth4th=document.getElementsByClassName("plus-hoverth4th")[0];
+
+plus_hover4th.style.transform="translate(0px, 0px)"
+plus_hoverth4th.style.transform="translate(0px, 0px)"
+
+
+plus_icon4th.onclick=function() {
+  if(plus_hover4th.style.transform=="translate(0px, 0px)") {
+    plus_hover4th.style.transform="translate(0px, -256px)";
+    plus_hoverth4th.style.transform="translate(0px, -256px)";
+    plus_hover4th.style.transition="all 0.4s ease-in-out";
+    plus_hoverth4th.style.transition="all 0.4s ease-in-out";
+    plus_icon4th.style.transform="rotate(45deg)";
+    plus_icon4th.style.transition="all 0.3s ease-in-out";
+  } else {
+    plus_hover4th.style.transform="translate(0px, 0px)";
+    plus_hoverth4th.style.transform="translate(0px, 0px)";
+    plus_icon4th.style.transform="rotate(0deg)";
+    plus_icon4th.style.transition="all 0.3s ease-in-out";
+  }
+}
+
+// FIFTH
+
+let plus_icon5th=document.getElementsByClassName("plus-icon5th")[0];
+let plus_hover5th=document.getElementsByClassName("plus-hover5th")[0];
+let plus_hoverth5th=document.getElementsByClassName("plus-hoverth5th")[0];
+
+plus_hover5th.style.transform="translate(0px, 0px)"
+plus_hoverth5th.style.transform="translate(0px, 0px)"
+
+
+plus_icon5th.onclick=function() {
+  if(plus_hover5th.style.transform=="translate(0px, 0px)") {
+    plus_hover5th.style.transform="translate(0px, -256px)";
+    plus_hoverth5th.style.transform="translate(0px, -256px)";
+    plus_hover5th.style.transition="all 0.4s ease-in-out";
+    plus_hoverth5th.style.transition="all 0.4s ease-in-out";
+    plus_icon5th.style.transform="rotate(45deg)";
+    plus_icon5th.style.transition="all 0.3s ease-in-out";
+  } else {
+    plus_hover5th.style.transform="translate(0px, 0px)";
+    plus_hoverth5th.style.transform="translate(0px, 0px)";
+    plus_icon5th.style.transform="rotate(0deg)";
+    plus_icon5th.style.transition="all 0.3s ease-in-out";
+  }
+}
+
+// SIXTH 
+
+let plus_icon6th=document.getElementsByClassName("plus-icon6th")[0];
+let plus_hover6th=document.getElementsByClassName("plus-hover6th")[0];
+let plus_hoverth6th=document.getElementsByClassName("plus-hoverth6th")[0];
+
+plus_hover6th.style.transform="translate(0px, 0px)"
+plus_hoverth6th.style.transform="translate(0px, 0px)"
+
+
+plus_icon6th.onclick=function() {
+  if(plus_hover6th.style.transform=="translate(0px, 0px)") {
+    plus_hover6th.style.transform="translate(0px, -256px)";
+    plus_hoverth6th.style.transform="translate(0px, -256px)";
+    plus_hover6th.style.transition="all 0.4s ease-in-out";
+    plus_hoverth6th.style.transition="all 0.4s ease-in-out";
+    plus_icon6th.style.transform="rotate(45deg)";
+    plus_icon6th.style.transition="all 0.3s ease-in-out";
+  } else {
+    plus_hover6th.style.transform="translate(0px, 0px)";
+    plus_hoverth6th.style.transform="translate(0px, 0px)";
+    plus_icon6th.style.transform="rotate(0deg)";
+    plus_icon6th.style.transition="all 0.3s ease-in-out";
   }
 }
