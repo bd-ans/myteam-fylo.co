efb8cc04051dda1fcdcaabf5e4a3763bbcb6b8b8
Elayt <elaytpatters@gmail.com>
2022-02-28 15:57:30 +0500

    add main functions

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let plus_icon=document.getElementById('plus_icon');
-let plus_hover=document.getElementById('plus_hover')
+let plus_icon=document.getElementById("plus-icon");
+let plus_hover=document.getElementById("plus-hover");
+let plus_hoverth=document.getElementById("plus-hoverth");
 
-function clickFunction(){
-    if(plus_hover.style.transform== "translate(0px, 0px)"){
-        plus_hover.style.transform="translate(0px, -100px)";
-    }else{
-        plus_hover.style.transform="translate(0px, 0px)";
-    }
+plus_hover.style.transform="translate(0px, 0px)"
+// plus_hoverth.style.transform="translate(0px, 0px)"
+
+plus_icon.onclick=function() {
+  if(plus_hover.style.transform=="translate(0px, 0px)") {
+    plus_hover.style.transform="translate(0px, -100px)";
+
+  } else {
+    plus_hover.style.transform="translate(0px, 0px)";
+  }
 }
