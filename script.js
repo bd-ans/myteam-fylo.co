c6805efe3e9fe50e7f358d43e22d9867d53e3824
Elayt <elaytpatters@gmail.com>
2022-02-28 17:24:56 +0500

    fixed mini bugs

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ let plus_hover=document.getElementById("plus-hover");
 let plus_hoverth=document.getElementById("plus-hoverth");
 
 plus_hover.style.transform="translate(0px, 0px)"
-// plus_hoverth.style.transform="translate(0px, 0px)"
+plus_hoverth.style.transform="translate(0px, 0px)"
+
 
 plus_icon.onclick=function() {
   if(plus_hover.style.transform=="translate(0px, 0px)") {
-    plus_hover.style.transform="translate(0px, -100px)";
+    plus_hover.style.transform="translate(0px, -256px)";
+    plus_hoverth.style.transform="translate(0px, -256px)";
+    plus_hover.style.transition="all 0.4s ease-in-out";
+    plus_hoverth.style.transition="all 0.4s ease-in-out";
+    plus_icon.style.transform="rotate(45deg)";
+    plus_icon.style.transition="all 0.3s ease-in-out";
 
   } else {
     plus_hover.style.transform="translate(0px, 0px)";
+    plus_hoverth.style.transform="translate(0px, 0px)";
+    plus_icon.style.transform="rotate(0deg)";
+    plus_icon.style.transition="all 0.3s ease-in-out";
+
   }
 }
